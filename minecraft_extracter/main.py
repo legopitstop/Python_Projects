80bb2ec3ce06f27fd6f317d7e9d6498e97d0954f
Legopitstop <1589lego@gmail.com>
2022-05-24 19:00:22 -0500

    Now uses customtkinter

--- minecraft_extracter/main.py
+++ minecraft_extracter/main.py
@@ -1,64 +1,67 @@
-from ctypes.wintypes import WORD
-from tkinter import DISABLED, E, EW, LEFT,NORMAL, RIGHT, W, BooleanVar, Button, Checkbutton, Entry, Frame, Label, OptionMenu, StringVar, Tk,LabelFrame, filedialog,messagebox,IntVar
+from tkinter import DISABLED, E, EW, LEFT,NORMAL, RIGHT, W, BooleanVar, Button, Checkbutton, Entry, Frame, Label, OptionMenu, StringVar,LabelFrame, filedialog,messagebox,IntVar
 from tkinter.ttk import Progressbar
-from aiohttp import streamer
-from tkstylesheet import TkThemeLoader
 from UserFolder import User
-import os,zipfile,threading,shutil,json,re,winsound,logging
+from customtkinter import CTk, CTkFrame, CTkLabel, CTkButton, CTkEntry,CTkCheckBox,CTkProgressBar
+import os,zipfile,threading,shutil,json,re,winsound,logging,customtkinter
+
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('blue')
 
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 
 user = User('com.legopitstop.minecraft_extracter')
 logging.basicConfig(format='[%(asctime)s] [%(name)s/%(levelname)s]: %(message)s', datefmt='%I:%M:%S', filename=os.path.join(user.get(),'latest.log'), filemode='w', encoding='utf-8', level=logging.DEBUG)
 
-class App():
+class App(CTk):
     def __init__(self):
-        self.APP = logging.getLogger('App')
+        super().__init__()
+
+        self.logger = logging.getLogger('App')
         if user.exists('config.json'):
-            self.APP.info('Loading config...')
+            self.logger.info('Loading config...')
             opn = user.open('config.json','r')
-            self.config = json.loads(opn.read())
+            self._config = json.load(opn)
             opn.close()
         else:
-            self.APP.info('Creating config...')
+            self.logger.info('Creating config...')
             versions = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft','versions')
             objects = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft','assets','objects')
             indexes = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft','assets','indexes')
             output = os.path.join(os.path.expanduser('~'),'Downloads')
 
-            self.config={"JarExtractor": {"enabled": True,"assets": True,"data": True,"versions": versions,"version": "unset" },"ObjectExtractor": {"enabled": False,"objects": objects,"indexes":indexes,"index":"unset"},"output": output,"show": True}
-            self.save_config(user,self.config)
+            self._config={"JarExtractor": {"enabled": True,"assets": True,"data": True,"versions": versions,"version": "unset" },"ObjectExtractor": {"enabled": False,"objects": objects,"indexes":indexes,"index":"unset"},"output": output,"show": True}
+            self.save_config(user,self._config)
 
-        root=Tk()
-        root.title('Minecraft Extractor')
-        root.iconbitmap(LOCAL+'/icon.ico')
-        root.resizable(False,False)
+        self.title('Minecraft Extractor')
+        self.iconbitmap(LOCAL+'/icon.ico')
+        # self.resizable(False,False)
+        self.protocol('WM_DELETE_WINDOW', self.exit)
 
         # Varables
         self.ENABLEJE=BooleanVar()
-        self.ENABLEJE.set(self.config['JarExtractor']['enabled'])
+        self.ENABLEJE.set(self._config['JarExtractor']['enabled'])
         self.ASSETS=BooleanVar()
-        self.ASSETS.set(self.config['JarExtractor']['assets'])
+        self.ASSETS.set(self._config['JarExtractor']['assets'])
         self.DATA=BooleanVar()
-        self.DATA.set(self.config['JarExtractor']['data'])
+        self.DATA.set(self._config['JarExtractor']['data'])
         self.SRC=StringVar()
-        self.SRC.set(self.config['JarExtractor']['version'])
+        self.SRC.set(self._config['JarExtractor']['version'])
         self.INDEX=StringVar()
-        self.INDEX.set(self.config['ObjectExtractor']['index'])
+        self.INDEX.set(self._config['ObjectExtractor']['index'])
         self.ENABLEOE=BooleanVar()
-        self.ENABLEOE.set(self.config['ObjectExtractor']['enabled'])
+        self.ENABLEOE.set(self._config['ObjectExtractor']['enabled'])
         self.OBJ=StringVar()
-        self.OBJ.set(self.config['ObjectExtractor']['objects'])
+        self.OBJ.set(self._config['ObjectExtractor']['objects'])
         self.DST=StringVar()
-        self.DST.set(self.config['output'])
+        self.DST.set(self._config['output'])
         self.SHOW=BooleanVar()
-        self.SHOW.set(self.config['show'])
+        self.SHOW.set(self._config['show'])
         self.PROGRESS=IntVar()
         self.PROGRESS.set(0)
 
         # Functions
         def get_versions():
-            ver = self.config['JarExtractor']['versions']
+            ver = self._config['JarExtractor']['versions']
             if os.path.exists(ver):
                 vers=['unset']
                 for v in os.listdir(ver):
@@ -76,7 +79,7 @@ class App():
                 return ["unset"]
 
         def get_indexes():
-            ver = self.config['ObjectExtractor']['indexes']
+            ver = self._config['ObjectExtractor']['indexes']
             if os.path.exists(ver):
                 ind=['unset']
                 for i in os.listdir(ver):
@@ -89,28 +92,6 @@ class App():
                 if opn == True:
                     user.show('config.json')
                 return ["unset"]
-
-        def update():
-            if self.ENABLEJE.get():
-                JE['fg'] = 'green'
-                src['state'] = NORMAL
-                assets['state'] = NORMAL
-                data['state'] = NORMAL
-            else:
-                JE['fg'] = 'black'
-                src['state'] = DISABLED
-                assets['state'] = DISABLED
-                data['state'] = DISABLED
-            if self.ENABLEOE.get():
-                OE['fg'] = 'green'
-                objEntry['state'] = NORMAL
-                objBtn['state'] = NORMAL
-                index['state'] = NORMAL
-            else:
-                OE['fg'] = 'black'
-                objEntry['state'] = DISABLED
-                objBtn['state'] = DISABLED
-                index['state'] = DISABLED
 
         def choose(varable,type):
             if type=='dir':
@@ -133,76 +114,98 @@ class App():
                 if path!='':
                     varable.set(path)
             else:
-                self.APP.warning('Unknown choose type "%s"',type)
+                self.logger.warning('Unknown choose type "%s"',type)
 
         def start():  
             threading.Thread(target=self.extract).start()
 
-        WRAPPER = Frame(root)
-        Label(WRAPPER,text='This tool will extract the Minecraft Jar or Unobficate Minecraft\'s objects using the index').grid(row=0,column=0,columnspan=2,sticky=W)
+        WRAPPER = CTkFrame(self)
+        CTkLabel(WRAPPER,text='This tool is for extracting Minecraft\'s assets and data').grid(row=0,column=0,columnspan=2,pady=5,padx=5,sticky=EW)
 
-        JE = LabelFrame(WRAPPER,text='Jar Extractor',pady=5,padx=5)
-        check = Frame(JE)
-        Checkbutton(check,text='Enable',variable=self.ENABLEJE,command=update).grid(row=0,column=0)
-        assets = Checkbutton(check,text='Assets',variable=self.ASSETS,command=update)
+        JE = CTkFrame(WRAPPER)
+        check = CTkFrame(JE)
+        CTkCheckBox(check,text='Enable',border_width=1,variable=self.ENABLEJE,command=self._update).grid(row=0,column=0)
+        assets = CTkCheckBox(check,text='Assets',border_width=1,variable=self.ASSETS,command=self._update)
         assets.grid(row=0,column=1)
-        data = Checkbutton(check,text='Data',variable=self.DATA,command=update)
+        data = CTkCheckBox(check,text='Data',border_width=1,variable=self.DATA,command=self._update)
         data.grid(row=0,column=2)
         check.grid(row=0,column=0,sticky=W)
-        Label(JE,text='The version to extract:').grid(row=1,column=0,sticky=W)
-        src = OptionMenu(JE,self.SRC,*get_versions())
-        src.grid(row=2,column=0,sticky=W)
-        JE.grid(row=1,column=0,pady=5,padx=5,sticky='nesw')
+        CTkLabel(JE,text='The version to extract:').grid(row=1,column=0,sticky=W)
+        self.src = OptionMenu(JE,self.SRC,*get_versions())
+        self.src.grid(row=2,column=0,sticky=W)
+        JE.grid(row=1,column=0,pady=2,padx=2,sticky='nesw')
 
-        OE = LabelFrame(WRAPPER,text='Object Extractor',pady=5,padx=5)
-        Checkbutton(OE,text='Enable',variable=self.ENABLEOE,command=update).grid(row=0,column=0,sticky=W)
-        Label(OE,text='The folder that contains all the objects:').grid(row=1,column=0,sticky=W)
-        objEntry = Entry(OE,textvariable=self.OBJ,width=65)
+        OE = CTkFrame(WRAPPER)
+        CTkCheckBox(OE,text='Enable',variable=self.ENABLEOE,border_width=1,command=self._update).grid(row=0,column=0,sticky=W)
+        CTkLabel(OE,text='The folder that contains all the objects:').grid(row=1,column=0,sticky=W)
+        objEntry = CTkEntry(OE,textvariable=self.OBJ,width=65)
         objEntry.grid(row=2,column=0,sticky=W)
-        objBtn = Button(OE,text='Browse...',command=lambda: choose(self.OBJ,'dir'))
+        objBtn = CTkButton(OE,text='Browse...',command=lambda: choose(self.OBJ,'dir'))
         objBtn.grid(row=2,column=1,padx=5,sticky=W)
-        Label(OE,text='The index version:').grid(row=3,column=0,sticky=W)
+        CTkLabel(OE,text='The index version:').grid(row=3,column=0,sticky=W)
         index = OptionMenu(OE,self.INDEX,*get_indexes())
         index.grid(row=4,column=0,sticky=W)
-        OE.grid(row=1,column=1,pady=5,padx=5,sticky='nesw')
-        WRAPPER.grid(row=0,column=0)
+        OE.grid(row=1,column=1,pady=2,padx=2,sticky='nesw')
 
-        GLOBE = Frame(root,pady=5,padx=5)
-        Label(GLOBE,text='Files will be extracted to this folder:').grid(row=0,column=0,sticky=W)
-        Entry(GLOBE,textvariable=self.DST,width=65).grid(row=1,column=0,sticky=W)
-        Button(GLOBE,text='Browse...',command=lambda: choose(self.DST,'dir')).grid(row=1,column=1,padx=5,sticky=W)
-        Checkbutton(GLOBE,text='Show extracted files when complete',variable=self.SHOW,onvalue=True,offvalue=False).grid(row=2,columnspan=2,column=0,sticky=W)
-        GLOBE.grid(row=1,column=0,sticky=W)
+        WRAPPER.grid(row=0,column=0,pady=5,padx=5,sticky=EW)
+
+        GLOBE = CTkFrame(self)
+        CTkLabel(GLOBE,text='Files will be extracted to this folder:').grid(row=0,column=0,sticky=W)
+        CTkEntry(GLOBE,textvariable=self.DST,width=65).grid(row=1,column=0,sticky=W)
+        CTkButton(GLOBE,text='Browse...',command=lambda: choose(self.DST,'dir')).grid(row=1,column=1,padx=5,sticky=W)
+        CTkCheckBox(GLOBE,text='Show extracted files when complete',variable=self.SHOW,onvalue=True,offvalue=False).grid(row=2,columnspan=2,column=0,sticky=W)
+        GLOBE.grid(row=1,column=0,pady=5,padx=5,sticky=EW)
         
         # Foot
-        foot = Frame(root,pady=5,padx=5)
-        label = Frame(foot)
-        self.PROGRESS_LABEL = Label(label,text='Waiting...')
-        self.PROGRESS_LABEL.object_id='foot'
+        foot = CTkFrame(self)
+        label = CTkFrame(foot)
+        self.PROGRESS_LABEL = CTkLabel(label,text='Waiting...')
         self.PROGRESS_LABEL.grid(row=0,column=0,sticky=W)
 
-        self.PROGRESS_FILE = Label(label)
-        self.PROGRESS_FILE.object_id='foot'
+        self.PROGRESS_FILE = CTkLabel(label)
         self.PROGRESS_FILE.grid(row=0,column=1,sticky=W)
 
-        label.object_id='foot'
         label.grid(row=0,column=0,sticky=EW)
 
-        self.PROGRESSBAR = Progressbar(foot,length=530,mode='determinate',maximum=100,variable=self.PROGRESS)
-        self.PROGRESSBAR.grid(row=1,column=0)
-        self.EXTRACTBTN = Button(foot,text='Extract',command=start)
-        self.EXTRACTBTN.grid(row=0,column=1,rowspan=2,padx=20,pady=10)
-        Button(foot,text='Cancel',command=root.destroy).grid(row=0,column=2,rowspan=2,padx=20,pady=10)
-        foot.object_id='foot'
-        foot.grid(row=2,column=0,sticky=W)
+        self.PROGRESSBAR = CTkProgressBar(foot,variable=self.PROGRESS)
+        self.PROGRESSBAR.grid(row=1,column=0,padx=2,sticky=EW)
+        self.EXTRACTBTN = CTkButton(foot,text='Extract',command=start)
+        self.EXTRACTBTN.grid(row=0,column=1,rowspan=2,padx=2,pady=2,sticky=E)
+        CTkButton(foot,text='Cancel',command=self.exit).grid(row=0,column=2,rowspan=2,padx=2,pady=2,sticky=E)
+        foot.grid(row=2,column=0,pady=5,padx=5,sticky=EW)
+
+        # Responsive
+        self.grid_columnconfigure(0,weight=1)
+        WRAPPER.grid_columnconfigure(0,weight=1)
+        WRAPPER.grid_columnconfigure(1,weight=1)
+        foot.grid_columnconfigure(0,weight=1)
 
         # Update
-        update()
+        self._update()
 
-        # Load theme
-        theme = TkThemeLoader(root)
-        theme.loadStyleSheet(LOCAL+'/theme.tkss')
-        root.mainloop()
+    def _update(self):
+        # if self.ENABLEJE.get():
+        #     self.src.configure(state=NORMAL)
+        #     self.assets.configure(state=NORMAL)
+        #     self.data.configure(state=NORMAL)
+        # else:
+        #     self.src.configure(state=DISABLED)
+        #     self.assets.configure(state=DISABLED)
+        #     self.data.configure(state=DISABLED)
+
+        # if self.ENABLEOE.get():
+        #     self.src.configure(state=NORMAL)
+        #     self.assets.configure(state=NORMAL)
+        #     self.data.configure(state=NORMAL)
+        # else:
+        #     self.src.configure(state=DISABLED)
+        #     self.assets.configure(state=DISABLED)
+        #     self.data.configure(state=DISABLED)
+        print('Update widgets')
+
+    def exit(self):
+        """Properly close the program"""
+        self.destroy()
 
     def status(self,text:str=None,filename:str=None,maximum:int=None,mode:str='determinate'):
         if text!=None: self.PROGRESS_LABEL['text']=text
@@ -223,7 +226,7 @@ class App():
 
     def save_config(self,user,config):
         # Update values
-        self.APP.info('Saving config...')
+        self.logger.info('Saving config...')
         try:
             config['JarExtractor']['enabled'] = self.ENABLEJE.get()
             config['JarExtractor']['assets'] = self.ASSETS.get()
@@ -308,17 +311,17 @@ class App():
 
     # Make seperate lib
     def extract(self):
-        self.save_config(user,self.config)
+        self.save_config(user,self._config)
         err=False
         # Create TEMP
         self.EXTRACTBTN['state'] = DISABLED
         if os.path.exists(LOCAL+'/_temp/')==False:
             os.makedirs(LOCAL+'/_temp/')
         if self.ENABLEJE.get()==True:
-            src =  os.path.join(self.config['JarExtractor']['versions'],self.SRC.get(),self.SRC.get()+'.jar')
+            src =  os.path.join(self._config['JarExtractor']['versions'],self.SRC.get(),self.SRC.get()+'.jar')
             if os.path.exists(src) and os.path.isfile(src) and self.SRC.get()!='unset':
                 # Extract
-                self.APP.info('Extracting jar...')
+                self.logger.info('Extracting jar...')
                 with zipfile.ZipFile(src) as zf:
                     total = len(zf.infolist())
                     self.status('Extracting Jar',maximum=total)
@@ -334,19 +337,19 @@ class App():
 
                 # Copying assets
                 if self.ASSETS.get()==True:
-                    self.APP.info('Copying assets')
+                    self.logger.info('Copying assets')
                     self.status('Copying assets')
                     self.copydir(LOCAL+'/_temp/output/assets',self.DST.get()+'/assets','filename')
                 # Copying data
                 if self.DATA.get()==True:
-                    self.APP.info('Copying data')
+                    self.logger.info('Copying data')
                     self.status('Copying data')
                     self.copydir(LOCAL+'/_temp/output/data',self.DST.get()+'/data','filename')
             else:
                 err='The source file is missing or isn\'t a valid file'
         
         if self.ENABLEOE.get()==True:
-            indexdir = self.config['ObjectExtractor']['indexes']
+            indexdir = self._config['ObjectExtractor']['indexes']
             index = os.path.join(indexdir,self.INDEX.get()+'.json')
             if os.path.exists(index) and os.path.isfile(index) and self.INDEX.get()!='unset':
                 DST = self.DST.get()
@@ -354,7 +357,7 @@ class App():
                 indx = json.load(opn)
                 opn.close()
                 # Copy objects
-                self.APP.info('Copying objects...')
+                self.logger.info('Copying objects...')
                 self.status('Copying objects')
                 src = self.OBJ.get()
                 dst = LOCAL+'\\_temp\\objects'
@@ -377,25 +380,29 @@ class App():
                     source = os.path.join(objects,hash)
                     dest=os.path.join(DST,key)
                     try: os.rename(source,dest)
-                    except: self.APP.warning('Failed: %s',key)
+                    except FileNotFoundError: self.logger.warning('Missing file "%s"',hash)
+                    except FileExistsError: self.logger.warning('File already exists "%s"',key)
+                    except: self.logger.exception('Failed: %s',key)
                     # Progressbar
                     self.PROGRESS.set(track)
                     track+=1
             else:
                 err='The source file is missing or isn\'t a valid file'
         
-        self.APP.info('Cleaning up...')
+        self.logger.info('Cleaning up...')
         self.cleanup(LOCAL+'/_temp')
         self.status('Complete!',filename='',maximum=100)
         self.PROGRESS.set(100)
-        self.APP.info('Complete!')
+        self.logger.info('Complete!')
         if err==False:
             self.EXTRACTBTN['state'] = NORMAL
             if self.SHOW.get()==True:
                 os.startfile(self.DST.get())
         else:
-            self.APP.error(err)
+            self.logger.error(err)
             messagebox.showerror('Error', err)
         winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
 
-app=App()
+if __name__=='__main__':
+    app=App()
+    app.mainloop()
